46b96a388f87bdbef773b37cffb5f25fe5bb26f8
CleitonQ <cleitonqueirozc@gmail.com>
2024-11-25 23:29:46 -0300

    Update 25112024 2

--- funcao_dobro/main.js
+++ funcao_dobro/main.js
@@ -1,5 +1,5 @@
 // Importamos ela de outro arquivo
-import { dobro } from `./fucoes.js`;
+import { dobro } from `./funcoes.js`;
 
 // Solicitamos ao usuário um número
 let numero = parseFloat(prompt("Digite um número:"));
@@ -7,7 +7,7 @@ let numero = parseFloat(prompt("Digite um número:"));
 if (!isNaN(numero)){
     // Se for um número válido, executamos a função dobro()
     let resultado = dobro(numero);
-    alert(`O dobro de ${numero} é ${resultado}`;)
+    alert(`O dobro de ${numero} é ${resultado}`);
 } else {
     alert("Por favor, insira um número válido.");
 }
