e08823e2d7b50655560f1ffc550c5c3f10ddaf34
CleitonQ <cleitonqueirozc@gmail.com>
2024-11-27 23:22:50 -0300

    Update 27112024 1

--- funcao_soma_e_subtracao/main.js
+++ funcao_soma_e_subtracao/main.js
@@ -1,13 +1,33 @@
-// Importamos ela de outro arquivo
-import { dobro } from `./funcoes.js`;
+// Importamos cada função
+import { dobro, somar, subtrair } from `./funcoes.js`;
 
-// Solicitamos ao usuário um número
-let numero = parseFloat(prompt("Digite um número:"));
-
-if (!isNaN(numero)){
-    // Se for um número válido, executamos a função dobro()
-    let resultado = dobro(numero);
-    alert(`O dobro de ${numero} é ${resultado}`);
-} else {
-    alert("Por favor, insira um número válido.");
+// Criamos algumas funções auxiliares para cada parte do programa
+function mostrarMenu() {
+    const opcao = prompt(`Escolha uma operação:\n1. Dobro de um número\n2. Somar dois números\n3. Subtrair dois números\n4. Sair`)
+    return parseInt(opcao)
 }
+
+function calcularDobro() {
+    let numero = parseFloat(prompt("Digite um número:"));
+
+    if (!isNaN(numero)) {
+        let resultado = dobro(numero);
+        alert(`O dobro de ${numero} é ${resultado}`);
+    } else {
+        alert("Por favor, insira um número válido.");
+    }
+}
+
+function calcularSoma() {
+    let numero1 = parseFloat(prompt("Digite o primerio número:"));
+    let numero2 = parseFloat(prompt("Digite o segundo número:"));
+
+    if (!isNaN(numero1) && !isNaN(numero2)){
+        let resultado = somar(numero1, numero2);
+        alert(`A soma de ${numero1} e ${numero2} é ${resultado}`);
+    } else {
+        alert("Por favor, insira números válidos.");
+    }
+}
+
+
